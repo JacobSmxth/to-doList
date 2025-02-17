c381f838f0ce0cf9a4130c87ce8fb453ff6b940c
JacobSmxth <jacob.d.smith@live.com>
2025-02-17 18:39:34 -0500

    Added remove and finish task functionality

--- script.js
+++ script.js
@@ -3,23 +3,33 @@ const taskInput = document.querySelector("#input-box")
 const taskButton = document.querySelector("#addTask")
 const taskList = document.querySelector("#listContainer")
 const taskCounter = document.querySelector("#counter")
+const deletePopup = document.querySelector("#deletePopup")
+
 
 let listCounter = 0
+let temp
 
+function l(v) {
+    console.log(v)
+}
 
+// Adding task event
 taskButton.addEventListener("click", () => {
     let inputText = taskInput.value
     const newTask = document.createElement('li')
-
+    const newDelete = document.createElement('span')
+    
     if (inputText) {
         listCounter++;
 
-        // TODO: Add some sort of element to delete task when user wants to.
-        // It needs to be given class name to style it correctly and some identifier to correctly delete the right task to delete. I'm thinking use the listCounter variable to keep things simple
         let taskText = document.createTextNode(inputText)
         newTask.append(taskText)
+        newTask.append(newDelete)
         taskList.append(newTask)
 
+
+        newDelete.classList.add("close")
+        newDelete.classList.add('item' + listCounter)
         newTask.id = "item" + listCounter
 
         taskInput.value = "";
@@ -34,3 +44,39 @@ taskButton.addEventListener("click", () => {
 
 });
 
+// Removing task event
+document.addEventListener("click", (e) => {
+    const item = e.target
+    const itemClass = item.classList
+
+
+    if(itemClass.contains("close")) {
+        
+        let listItemID = itemClass[1]
+        const removedItem = document.getElementById(listItemID)
+        temp = removedItem.innerHTML
+        removedItem.remove()
+        listCounter--
+        taskCounter.innerText  = " " + listCounter
+    }
+
+})
+
+// Finish task 
+document.addEventListener('click', (e) => {
+    const item = e.target;
+    const selectItem = document.getElementById(item.id)
+
+    
+    switch (selectItem.className) {
+        case "":
+            selectItem.className = "checked"
+            break;
+        case "checked":
+            selectItem.className = ""
+            break;
+        default:
+            l("failed")
+            break;
+    }
+})
